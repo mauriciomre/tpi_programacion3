00380ca18c7b35f824923629487f4788ea9add85
unknown <lautaroolivero44@gmail.com>
2025-11-15 17:36:16 -0300

    LocalStorage, login y registro, verificar hrefs

--- assets/js/controllers/authController.js
+++ assets/js/controllers/authController.js
@@ -1,15 +1,90 @@
 // src/controllers/authController.js
-import { getUserByEmail } from "../models/userModel.js";
-import { User } from "../models/User.js";
 
-export async function handleLogin(email, password) {
-    const data = await getUserByEmail(email);
-    if (!data || data.password !== password) {
-        alert("Usuario o contraseña incorrectos");
-        return;
-    }
+/*
 
     const user = new User(data); // creás la instancia de la clase
     localStorage.setItem("user", JSON.stringify(user));
     alert(`Bienvenido ${user.nombre}`);
+}
+*/
+
+import { userService } from "../services/apiServices";
+import { saveUser } from "../services/storageService";
+
+//LOGIN (VERIFICAR LOS HREF)
+
+export async function handleLogin(email, password) {
+    console.log(`Intentando logear con ${email}`);
+
+    //validar mail y pass
+    
+    if (email === '' || password === ''){
+        alert('No puedes dejar campos vacios');
+        return false;
+    }
+
+    try{
+        const user = await userService.getByEmail(email);
+
+        if (user === null){
+            alert('Usuario no encontrado');
+            return false;
+        }
+
+        if (user.password !== password){
+            alert('Contrasenia Incorrecta');
+            return false;
+        }
+        // login exitoso
+        console.log(`Login exitoso: ${user}`)
+        saveUser(user);
+        alert(`Bienvenid@ ${user.nombre}`);
+
+        if (user.role === 'ADMIN'){
+            window.location.href = '#/loquesea'
+        }else{
+            window.localStorage.href ='#/reservations'
+        }
+        
+        return true;
+
+    } catch (error){
+        console.error(`Error de login: ${error}`);
+        alert('Error al iniciar seesion.');
+        return false;
+    }
+}
+
+//REGISTRO
+
+export async function handleRegister(nombre, email, password) {
+    console.log (`intentando registrar con ${email} y ${nombre}`);
+
+     if (nombre === '' || email === '' || password === '') {
+        alert('No puedes dejar campos vacios');
+        return false;
+    }
+
+    // Se intenta crear user con role USUARIO por defecto
+    try{
+         const existeUsuario = await userService.create({
+            nombre: nombre,
+            email: email,
+            password: password,
+            role: 'USUARIO'
+         });
+
+         console.log('Usuario creado:', newUser);
+
+         saveUser(newUser);
+         alert(`Bienvenido ${newUser.nombre}`);
+
+         window.location.href = '#/reservations';
+
+         return true;
+    } catch (error){
+        console.error(`Error de login: ${error}`);
+        alert('Error al registrar usuario.');
+        return false;
+    }
 }
---
+++ assets/js/services/storageService.js
@@ -0,0 +1,67 @@
+//MODIFICAR EL MOCKAPI EL TIPO DE DATO DE ROLE (ACTUALMENTE ESTA DANDO BOOLEANO y tiene de dar STRING: USUARIO o ADMIN)
+
+//Usuario en el navegador
+const STORAGE_KEY = 'usuarioActual';
+
+//recibir objeto user, convertirlo a texto y guardar con la clave STORAGE_KEY
+export function saveUser(user) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(user));
+
+}
+
+//Trae user logueado desde localStorage
+export function getActualUser() {
+    const userJson = localStorage.getItem(STORAGE_KEY);
+    if (userJson === null) {
+        console.log('No hay un usuario logueado')
+        return null;
+    } else {
+        return JSON.parse(userJson)
+    }
+}
+
+//verificar sesion, devuelve true o false
+export function isAuthenticated() {
+    return getActualUser() !== null;
+}
+
+//verificar ADMIN, devuelve true o false
+export function esAdmin() {
+    const user = getActualUser();
+    if (user === null) {
+        return false;
+    }
+    return user.role === 'admin';
+}
+
+//UTILIZANDO WINDOW BOM (verificar)(verificar)(verificar)
+
+export function logout() {
+    localStorage.removeItem(STORAGE_KEY);
+    window.location.href = '#/login';
+}
+// funcion para devolver a login si isAuthenticated() es false
+export function requiereAuth() {
+    if (!isAuthenticated()) {
+        alert('Debes iniciar sesion...');
+        window.location.href = '#/login';
+        return false;
+    }
+    return true;
+}
+
+// verificar admin. FALTA terminar de completar el href FALTA terminar de completar el href
+
+export function requiereAdmin(){
+    if (!isAuthenticated()) {
+        alert('Debes iniciar sesion...');
+        window.location.href = '#/login';
+        return false;
+    }
+    if (!isAdmin()) {
+        alert('No tienes permisos de administrador');
+        window.location.href = '#/';
+        return false;
+    }
+    return true;
+}
