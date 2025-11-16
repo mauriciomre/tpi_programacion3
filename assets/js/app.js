b911f53c70007b59100db718da225370a1376b6e
LucianaSassia <sassialuciana@gmail.com>
2025-11-16 20:07:11 -0300

    Actualizo estilos, login, register, index y agrego imágenes y página inicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,11 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const app = document.getElementById("app");
     const router = new Router(app);
 
+    router.addRoute("", "pages/inicio.html");
+    router.addRoute("inicio", "pages/inicio.html");
     router.addRoute("login", "pages/login.html");
     router.addRoute("reservas", "pages/reservations.html");
     router.addRoute("dashboard", "pages/dashboard.html");
     router.addRoute("registro", "pages/register.html");
     router.addRoute("habitaciones", "pages/rooms.html");
+
 
     // 👇 Se ejecuta DESPUÉS de cada carga de vista
     router.onPageLoaded = (path) => {
